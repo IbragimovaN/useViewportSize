164513a36e7dccfd1405be3919cd916de46faf0e
Anastasiya <anastasiya.ibr@mail.ru>
2024-11-20 22:01:34 +0300

    feat:useViewportSize готов, использует useThrottle и useWindowEvent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Demo } from "./Demo";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Demo />
+    </div>
+  );
 }
 
 export default App;
